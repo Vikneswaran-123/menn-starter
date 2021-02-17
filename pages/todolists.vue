d42533682e055a3c33ea46de52eb1b9516a96b2b
Vikneswaran-123 <viki.ratnam@gmail.com>
2021-02-17 11:53:26 +0530

    initial commit(Tailwind config)

---
+++ pages/todolists.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <div>
+      <div>
+        <span> Todo Lists </span>
+      </div>
+      <div>
+        <input type="text" placeholder="Add Content" />
+      </div>
+    </div>
+  </div>
+</template>
